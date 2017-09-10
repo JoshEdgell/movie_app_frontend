1c47b7bd6b4eac45e5eae9b4477cf9b4710ac2cf
JoshEdgell <josh.0.edgell@gmail.com>
2017-09-10 19:41:32 -0400

    app.js now pulls from either database, depending on whether or not the movie is in our database or not

--- public/app.js
+++ public/app.js
@@ -2,18 +2,23 @@ const app = angular.module('movies', []);
 
 app.controller('MainController', ['$http', function($http){
   const controller = this;
+  this.url = 'http://localhost:3000/'
   this.allMovies = [];
   this.allUsers = [];
   this.searchResults = [];
   this.currentMovie = {};
   this.requestedMovieId = 0;
   this.displaySearchResults = false;
-  this.displayAddReviewToMovie = false;
+  this.displaySingleMovie = false;
+  this.hideAllCenterDivs = function(){
+    this.displaySearchResults = false;
+    this.displaySingleMovie = false;
+  };
   this.getAllApiMovies = function(){
     // Return the data for all of the movies in our database
     $http({
       method: 'get',
-      url: 'http://localhost:3000/movies'
+      url: this.url + 'movies'
     }).then(function(response){
       controller.allMovies = response.data;
     })
@@ -22,19 +27,18 @@ app.controller('MainController', ['$http', function($http){
     // Returns an array of all of the users in our database (sans passwords);
     $http({
       method: 'get',
-      url: 'http://localhost:3000/users'
+      url: this.url + 'users'
     }).then(function(response){
       response.data.forEach(function(a){delete a.password});
       controller.allUsers = response.data;
     }, function(error){
       console.log(error, 'getAllUsers')
     })
-  }
+  };
   this.checkMovieList = function(input){
       // Check the name of a movie to see if it's in our database - returns 'false' if not, returns movie_id if it is in the database
     for (let i = 0; i < this.allMovies.length; i++){
-      if (this.allMovies[i].title == input){
-        this.requestedMovieId = this.allMovies[i].id
+      if (this.allMovies[i].imdbid == input){
         return true
       }
     }
@@ -52,18 +56,32 @@ app.controller('MainController', ['$http', function($http){
       console.log(error, 'searchAllNames');
     })
   };
-  this.selectMovieToAddReview = function(imdb_id){
+  this.selectMovieToAddReview = function(imdbid){
     //When a user clicks on a found movie poster, this function will use the movie's imdbID to find it either in our database or in OMDB's.  It'll set all other center divs to false and display the addReviewToMovie div.
-    if(this.checkMovieList(imdb_id) == true){
-      console.log('in database');
+    if(this.checkMovieList(imdbid) == true){
       // If the movie is already in our database, send a request to our database to get the movie's data into the controller's currentMovie object.
+      let id = 0;
+      for (let i = 0; i < this.allMovies.length; i++){
+        if (this.allMovies[i].imdbid === imdbid){
+          id = this.allMovies[i].id
+        }
+      }
+      $http({
+        method: 'get',
+        url: this.url + 'movies/' + id
+      }).then(function(response){
+        controller.currentMovie = response.data;
+        controller.hideAllCenterDivs();
+        controller.displaySingleMovie = true;
+      }, function(error){
+        console.log(error);
+      })
     } else {
       // If the movie is not in our database, send a request to OMDB to get the movie's data and put the relevant data into the controller's currentMovie object.
       $http({
         method: 'get',
-        url: 'http://www.omdbapi.com/?i=' + imdb_id + '&apikey=68da6914'
+        url: 'http://www.omdbapi.com/?i=' + imdbid + '&apikey=68da6914'
       }).then(function(response){
-        console.log(response.data);
         controller.currentMovie.title = response.data.Title;
         controller.currentMovie.rating = response.data.Rated;
         controller.currentMovie.genre = response.data.Genre;
@@ -73,12 +91,26 @@ app.controller('MainController', ['$http', function($http){
         controller.currentMovie.poster = response.data.Poster;
         controller.currentMovie.plot = response.data.Plot;
         controller.currentMovie.rotten_tomatoes_score = response.data.Ratings[1].Value;
-        controller.currentMovie.imdb_id = response.data.imdbID;
-        console.log(controller.currentMovie, "current movie")
+        // console.log(response.data.imdbID, 'imdbID');
+        controller.currentMovie.imdbid = response.data.imdbID;
+        controller.hideAllCenterDivs();
+        controller.displaySingleMovie = true;
       }, function(error){
         console.log(error);
       })
     }
+  };
+  this.addMovieToDatabase = function(){
+    this.hideAllCenterDivs();
+    $http({
+      method: 'post',
+      url: this.url + 'movies',
+      data: this.currentMovie
+    }).then(function(response){
+      console.log(response, 'response after add movie')
+    }, function(error){
+      console.log(error)
+    })
   };
 
 
